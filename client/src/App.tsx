f8d8d017ab1564e8ed10e472dbdeac07d6766798
ShowravKormokar <showravkormokar354@gmail.com>
2025-09-30 02:18:47 +0600

    Make a hero section and animate on split text, content using gsap timeline and scroll trigger

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Navbar from "./components/Navbar"
+import HeroSection from "./sections/HeroSection"
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
         <main>
             <Navbar />
+            <HeroSection />
+            <div className="h-dvh border border-red-400"></div>
         </main>
     )
 }
