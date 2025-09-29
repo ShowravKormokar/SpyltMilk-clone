dfdff916f89a208f54d5c84b949ab2032af2ff25
ShowravKormokar <showravkormokar354@gmail.com>
2025-09-30 02:33:29 +0600

    Fix some issue and warnings in the hero section

--- client/src/sections/HeroSection.tsx
+++ client/src/sections/HeroSection.tsx
@@ -5,46 +5,46 @@ import { SplitText } from "gsap/all";
 const HeroSection = () => {
 
     useGSAP(() => {
-        const titleSplit = SplitText.create(".hero-title", {
-            type: "chars",
+        document.fonts.ready.then(() => {
+            const titleSplit = SplitText.create(".hero-title", { type: "chars" });
+
+            const tl = gsap.timeline({ delay: 1 });
+
+            tl.to(".hero-content", {
+                opacity: 1,
+                y: 0,
+                ease: "power1.inOut"
+            })
+                .to(".hero-text-scroll", {
+                    duration: 1,
+                    clipPath: "polygon(0% 0%,100% 0%,100% 100%, 0% 100%)",
+                    ease: "circ.out"
+                }, "-=0.5")
+                .from(titleSplit.chars, {
+                    yPercent: 200,
+                    stagger: 0.02,
+                    ease: "power2.out"
+                }, "-=0.5");
+
+            const heroTl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".hero-container",
+                    start: "1% top",
+                    end: "bottom top",
+                    scrub: true,
+                    // markers: true
+                }
+            });
+
+            heroTl.to(".hero-container", {
+                rotate: 7,
+                scale: 0.9,
+                yPercent: 30,
+                ease: "power1.inOut"
+            });
         });
+    });
 
-        const tl = gsap.timeline({
-            delay: 1
-        });
-
-        tl.to(".hero-content", {
-            opacity: 1,
-            y: 0,
-            ease: "power1.inOut"
-        }).to(".hero-text-scroll", {
-            duration: 1,
-            clipPath: "polygon(0% 0%,100% 0%,100% 100%, 0% 100%)",
-            ease: "circ.out"
-        }, "-=0.5")
-            .from(titleSplit.chars, {
-                yPercent: 200,
-                stagger: 0.02,
-                ease: "power2.out"
-            }, "-=0.5");
-
-        const heroTl = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".hero-container",
-                start: "1% top",
-                end: "bottom top",
-                scrub: true,
-                // markers: true
-            }
-        });
-
-        heroTl.to(".hero-container", {
-            rotate: 7,
-            scale: 1,
-            yPercent: 30,
-            ease: "power1.inOut"
-        })
-    })
 
     return (
         <section>
@@ -54,10 +54,7 @@ const HeroSection = () => {
                     <div className="overflow-hidden">
                         <h1 className="hero-title">Freaking Delicious</h1>
                     </div>
-                    <div
-                        style={{ clipPath: "polygon(50% 0,50% 0,50% 100%, 50% 100%", }}
-                        className="hero-text-scroll"
-                    >
+                    <div className="hero-text-scroll">
                         <div className="hero-subtitle">
                             <h1>Protein + Caffine</h1>
                         </div>
