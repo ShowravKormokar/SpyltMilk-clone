4cc004a76fb0362efc256fe0ea24f874a8e240dc
ShowravKormokar <showravkormokar354@gmail.com>
2025-09-29 23:55:26 +0600

    Import all resources

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,11 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <h1 className='text-white'>Hello, World!</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className='text-white'>Hello, World!</h1>
+        </>
+    )
 }
 
 export default App
