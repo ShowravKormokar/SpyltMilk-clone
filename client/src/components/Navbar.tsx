24315859fae71742d4ab1a5f62192838b742cf5e
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-01 11:02:04 +0600

    fix image path

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return (
         <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
-            <img src="/public/images/nav-logo.svg" alt="navbar-logo" className="md:w-24 w-20" />
+            <img src="/images/nav-logo.svg" alt="navbar-logo" className="md:w-24 w-20" />
         </nav>
     )
 }
--- client/src/sections/HeroSection.tsx
+++ client/src/sections/HeroSection.tsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
     return (
         <section>
             <div className="hero-container">
-                <img src="/public/images/static-img.png" alt="" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150" />
+                <img src="/images/static-img.png" alt="" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150" />
                 <div className="hero-content opacity-0">
                     <div className="overflow-hidden">
                         <h1 className="hero-title">Freaking Delicious</h1>
