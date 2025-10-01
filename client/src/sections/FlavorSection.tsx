aff0305c6097b7d3bde9bc6e13a03bceff0f2383
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-01 15:20:39 +0600

    Create flavor section basic structure

---
+++ client/src/sections/FlavorSection.tsx
@@ -0,0 +1,15 @@
+
+const FlavorSection = () => {
+    return (
+        <section className="flavor-section">
+            <div className="h-full flex lg:flex-row flex-col items-center relative">
+                <div className="lg:w-[57%] flex-none h-80 lg:h-full">
+
+                </div>
+                <div className="div"></div>
+            </div>
+        </section>
+    )
+}
+
+export default FlavorSection;
