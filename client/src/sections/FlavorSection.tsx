9719095c693277476b72346b3ebab26e59b555a0
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-02 12:06:11 +0600

    Disable the button

--- client/src/sections/FlavorSection.tsx
+++ client/src/sections/FlavorSection.tsx
@@ -3,7 +3,7 @@ import FlavorTitle from "../components/FlavorTitle";
 
 const FlavorSection = () => {
     return (
-        <section className=" relative">
+        <section className="">
             <div className="flavor-section">
                 <div className="h-dvh flex lg:flex-row flex-col relative">
                     <div className="lg:w-[57%] flex-none h-80 lg:h-full  xl:mt-0">
@@ -13,11 +13,12 @@ const FlavorSection = () => {
                         <FlavorSlider />
                     </div>
                 </div>
-
             </div>
-            <div className="max-w-full w-[15vw] h-auto text-center flex flex-col items-center justify-center relative bottom-20 z-100">
-                <button type="button" className="text-md rounded-4xl bg-amber-400 px-8 py-2 cursor-pointer absolute">Get It Now</button>
-            </div>
+            {/* <div className="relative">
+                <div className="max-w-full w-[15vw] h-auto text-center flex flex-col items-center justify-center bottom-20 z-100 absolute">
+                    <button type="button" className="text-md rounded-4xl bg-amber-400 px-8 py-2 cursor-pointer ">Get It Now</button>
+                </div>
+            </div> */}
         </section>
     )
 }
