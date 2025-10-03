c47c16648e15e1cdf85ec7567867d3335117a677
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-03 21:47:52 +0600

    Design perfectly nutritiion section

--- client/src/sections/NutritionSection.tsx
+++ client/src/sections/NutritionSection.tsx
@@ -5,46 +5,46 @@ const NutritionSection = () => {
         <section className="nutrition-section">
             <img src="/images/slider-dip.png" alt="" className="w-full object-cover z-0" />
             <img src="/images/big-img.png" alt="" className="big-img" />
-            <div className="flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0">
-                <div className="relative inline-block md:translate-y-20">
-                    <div className="general-title relative flex flex-col justify-center items-center gap-24">
-                        <div className="overflow-hidden place-self-start">
-                            <h1>It still does</h1>
-                        </div>
-                        <div className="nutrition-text-scroll place-self-start">
-                            <div className="bg-yellow-brown pb-5 md:pt-0 pt-3 md:px-5 px-3 inline-block">
-                                <h2 className="text-milk-yellow">Body Good</h2>
+            <div className="flex flex-col justify-center">
+                <div className="flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0">
+                    <div className="relative inline-block md:translate-y-20">
+                        <div className="general-title relative flex flex-col justify-center items-center gap-24">
+                            <div className="overflow-hidden place-self-start">
+                                <h1>It still does</h1>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex md:justify-end items-start translate-y-20">
-                    <div className="md:max-w-xs max-w-md">
-                        <p className="text-sm md:text-right text-balance font-paragraph">Milk contains a wide array of nutrients including vitamins, minerals, and proteins and that is lactose free.</p>
-                    </div>
-                </div>
-            </div>
-
-
-
-            <div className="nutrition-box">
-                <div className="list-wrapper">
-                    {
-                        nutrientLists.map((nutrients, index) => (
-                            <div key={index} className="relative flex-1 col-center">
-                                <div className="">
-                                    <p className="md:text-lg font-paragraph">{nutrients.label}</p>
-                                    <p className="font-paragraph text-sm mt-2">up to</p>
-                                    <p className="text-2xl md:text-2xl tracking-tighter font-bold">{nutrients.amount}</p>
+                            <div className="nutrition-text-scroll place-self-start">
+                                <div className="bg-yellow-brown pb-5 md:pt-0 pt-3 md:px-5 px-3 inline-block">
+                                    <h2 className="text-milk-yellow">Body Good</h2>
                                 </div>
-                                {
-                                    index !== nutrientLists.length - 1 && (
-                                        <div className="spacer-border" />
-                                    )
-                                }
                             </div>
-                        ))
-                    }
+                        </div>
+                    </div>
+                    <div className="flex md:justify-end items-start translate-y-20">
+                        <div className="md:max-w-xs max-w-md">
+                            <p className="text-sm md:text-right text-balance font-paragraph">Milk contains a wide array of nutrients including vitamins, minerals, and proteins and that is lactose free.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="nutrition-box">
+                    <div className="list-wrapper">
+                        {
+                            nutrientLists.map((nutrients, index) => (
+                                <div key={index} className="relative flex-1 col-center">
+                                    <div className="">
+                                        <p className="md:text-sm font-paragraph">{nutrients.label}</p>
+                                        <p className="font-paragraph text-[10px] mt-[6px]">up to</p>
+                                        <p className="text-2xl md:text-2xl tracking-tighter font-bold">{nutrients.amount}</p>
+                                    </div>
+                                    {
+                                        index !== nutrientLists.length - 1 && (
+                                            <div className="spacer-border" />
+                                        )
+                                    }
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </section>
