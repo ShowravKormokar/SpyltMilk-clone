8ab4a9b9bf0a331b658629127341927efd0a7490
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-16 23:44:41 +0600

    Make button responsive

--- client/src/sections/FlavorSection.tsx
+++ client/src/sections/FlavorSection.tsx
@@ -13,40 +13,57 @@ const FlavorSection = () => {
     const btnMobile = useMediaQuery({ query: "(max-width: 768px)", });
     if (btnMobile) {
         useGSAP(() => {
+            const btn = document.querySelector<HTMLDivElement>(".fixed-btn");
+            if (!btn) return;
+            const sliderCon = document.querySelector<HTMLDivElement>(".slider-con");
+            if (!sliderCon) return;
 
             if (!flavorRef.current) return;
 
             const hScrollAmount = flavorRef.current.scrollHeight - window.innerHeight;
+            // console.log(hScrollAmount);
             gsap.to(".fixed-btn", {
-                bottom: 0,
+                opacity: 1,
                 scrollTrigger:
                 {
-                    trigger: ".fixed-btn",
+                    trigger: ".flavor-section",
                     start: "top 90%",
-                    end: `1550% top`,
-                    // end: `${hScrollAmount}px top`,
-                    pin: true,
-                    markers: true
+                    // end: `2000px top`,
+                    end: `${hScrollAmount}px top`,
+                    // pin: true,
+                    onUpdate: (self) => {
+                        const progress = self.progress; // 0 to 1
+                        if (progress < 1) {
+                            btn.style.position = "fixed";
+                            btn.style.top = "90%";
+                            sliderCon.style.padding = "0%";
+                        } else {
+                            btn.style.position = "absolute";
+                            btn.style.bottom = "0%";
+                            sliderCon.style.padding = "5rem";
+                        }
+                    },
+                    markers: true,
+                    preventOverlaps: true
                 }
             })
-        }, [flavorRef]);
+        });
     }
     return (
         <section ref={flavorRef} className="flavor-section relative overflow-hidden">
             {/* Fixed button (stays in bottom-center during scroll) */}
             <div
-                className={`${btnMobile ? "fixed-btn w-full py-7 h-25 absolute  z-[100] flex justify-center bg-milk" : "absolute bottom-[5%] left-1/2 -translate-x-1/2 z-[100] flex justify-center"}`}>
+                className={`${btnMobile ? "opacity-0 fixed-btn w-full py-7 h-25 left-1/2 -translate-x-1/2 z-[100] flex justify-center bg-milk" : "absolute bottom-[5%] left-1/2 -translate-x-1/2 z-[100] flex justify-center"}`}>
                 <button type="button" className="text-md rounded-4xl bg-amber-400 px-8 py-2 cursor-pointer shadow-md hover:bg-amber-500 transition-all" >
                     Get It Now
                 </button>
             </div>
-
             {/* This container moves horizontally */}
             <div className="flavor-scroll-inner h-full flex lg:flex-row flex-col relative">
                 <div className="lg:w-[57%] flex-none h-80 lg:h-full lg:mt-[15%] xl:mt-0 lg:pb-50">
                     <FlavorTitle />
                 </div>
-                <div className="lg:pb-0 pb-20">
+                <div className="lg:pb-0 slider-con">
                     <FlavorSlider />
                 </div>
             </div>
