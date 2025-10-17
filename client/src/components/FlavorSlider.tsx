cbedbecb56389d9da60b0d97164affab061dc06a
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-17 16:27:42 +0600

    Separate gsap code to it's specific components

--- client/src/components/FlavorSlider.tsx
+++ client/src/components/FlavorSlider.tsx
@@ -1,8 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import { flavorlists } from "../constants/details";
 import gsap from "gsap";
-import { useRef } from "react";
-import { useMediaQuery } from "react-responsive";
 
 // At the top of component file
 // Glob import all images and videos
@@ -18,41 +16,8 @@ const getImage = (fileName: string): string => {
 };
 
 const FlavorSlider = () => {
-    const slideRef = useRef<HTMLDivElement | null>(null);
-
-    const isTablet = useMediaQuery({
-        query: "(max-width: 1024px)",
-    });
-    const isMob = useMediaQuery({
-        query: "(max-width: 768px)",
-    });
 
     useGSAP(() => {
-        //Card Slider animation (Horizontally)
-        if (!slideRef.current) return;
-
-        const scrollAmount = slideRef.current.scrollWidth - window.innerWidth;
-
-        if (!isTablet) {
-            const tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: ".flavor-section",
-                    start: "top 0%",
-                    end: "+=4000",
-                    scrub: true,
-                    pin: true,
-                    // markers: true
-                },
-            });
-
-            tl.to(".flavor-scroll-inner", {
-                x: isMob ? 0 : `-${scrollAmount}px`,
-                y: isMob ? `${scrollAmount}px` : 0,
-                ease: "power1.inOut",
-            });
-
-        }
-
         //Element hover
         const cards = document.querySelectorAll<HTMLDivElement>(".flavors > div");
 
@@ -87,7 +52,7 @@ const FlavorSlider = () => {
     });
 
     return (
-        <div ref={slideRef} className="slider-wrapper lg:w-[480vw] lg:h-full mt-0 xl:mt-0 bg-milk h-[100%]">
+        <div className="slider-wrapper lg:w-[480vw] lg:h-full mt-0 xl:mt-0 bg-milk h-[100%]">
             <div className="flavors lg:pb-50 flex md:flex-row flex-col items-center lg:items-start lg:pt-10 2xl:gap-72 lg:gap-52 md:gap-24 gap-7 flex-nowrap">
                 {flavorlists.map((flavor) => (
                     <div
--- client/src/sections/FlavorSection.tsx
+++ client/src/sections/FlavorSection.tsx
@@ -9,46 +9,82 @@ import { useRef } from "react";
 gsap.registerPlugin(ScrollTrigger);
 
 const FlavorSection = () => {
+
+    const slideRef = useRef<HTMLDivElement | null>(null);
+
+    const isTablet = useMediaQuery({
+        query: "(max-width: 1024px)",
+    });
+    const isMob = useMediaQuery({
+        query: "(max-width: 768px)",
+    });
+
+    useGSAP(() => {
+        //Card Slider animation (Horizontally)
+        if (!slideRef.current) return;
+
+        const scrollAmount = slideRef.current.scrollWidth - window.innerWidth;
+
+        if (!isTablet) {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".flavor-section",
+                    start: "top 0%",
+                    end: "+=4000",
+                    scrub: true,
+                    pin: true,
+                    // markers: true
+                },
+            });
+
+            tl.to(".flavor-scroll-inner", {
+                x: isMob ? 0 : `-${scrollAmount}px`,
+                y: isMob ? `${scrollAmount}px` : 0,
+                ease: "power1.inOut",
+            });
+
+        }
+    })
     const flavorRef = useRef<HTMLDivElement | null>(null);
     const btnMobile = useMediaQuery({ query: "(max-width: 768px)", });
-    if (btnMobile) {
-        useGSAP(() => {
-            const btn = document.querySelector<HTMLDivElement>(".fixed-btn");
-            if (!btn) return;
-            const sliderCon = document.querySelector<HTMLDivElement>(".slider-con");
-            if (!sliderCon) return;
+    // if (btnMobile) {
+    //     useGSAP(() => {
+    //         const btn = document.querySelector<HTMLDivElement>(".fixed-btn");
+    //         if (!btn) return;
+    //         const sliderCon = document.querySelector<HTMLDivElement>(".slider-con");
+    //         if (!sliderCon) return;
 
-            if (!flavorRef.current) return;
+    //         if (!flavorRef.current) return;
 
-            const hScrollAmount = flavorRef.current.scrollHeight - window.innerHeight;
-            // console.log(hScrollAmount);
-            gsap.to(".fixed-btn", {
-                opacity: 1,
-                scrollTrigger:
-                {
-                    trigger: ".flavor-section",
-                    start: "top 90%",
-                    // end: `2000px top`,
-                    end: `${hScrollAmount}px top`,
-                    // pin: true,
-                    onUpdate: (self) => {
-                        const progress = self.progress; // 0 to 1
-                        if (progress < 1) {
-                            btn.style.position = "fixed";
-                            btn.style.top = "90%";
-                            sliderCon.style.padding = "0%";
-                        } else {
-                            btn.style.position = "absolute";
-                            btn.style.bottom = "0%";
-                            sliderCon.style.padding = "5rem";
-                        }
-                    },
-                    markers: true,
-                    preventOverlaps: true
-                }
-            })
-        });
-    }
+    //         const hScrollAmount = flavorRef.current.scrollHeight - window.innerHeight;
+    //         // console.log(hScrollAmount);
+    //         gsap.to(".fixed-btn", {
+    //             opacity: 1,
+    //             scrollTrigger:
+    //             {
+    //                 trigger: ".flavor-section",
+    //                 start: "top 90%",
+    //                 // end: `2000px top`,
+    //                 end: `${hScrollAmount}px top`,
+    //                 // pin: true,
+    //                 onUpdate: (self) => {
+    //                     const progress = self.progress; // 0 to 1
+    //                     if (progress < 1) {
+    //                         btn.style.position = "fixed";
+    //                         btn.style.top = "90%";
+    //                         sliderCon.style.padding = "0%";
+    //                     } else {
+    //                         btn.style.position = "absolute";
+    //                         btn.style.bottom = "0%";
+    //                         sliderCon.style.padding = "5rem";
+    //                     }
+    //                 },
+    //                 markers: true,
+    //                 preventOverlaps: true
+    //             }
+    //         })
+    //     });
+    // }
     return (
         <section ref={flavorRef} className="flavor-section relative overflow-hidden">
             {/* Fixed button (stays in bottom-center during scroll) */}
@@ -60,10 +96,10 @@ const FlavorSection = () => {
             </div>
             {/* This container moves horizontally */}
             <div className="flavor-scroll-inner h-full flex lg:flex-row flex-col relative">
-                <div className="lg:w-[57%] flex-none h-80 lg:h-full lg:mt-[15%] xl:mt-0 lg:pb-50">
+                <div className="lg:w-[57%] flex-none h-80 lg:h-full lg:mt-[9%] xl:mt-0 lg:pb-50">
                     <FlavorTitle />
                 </div>
-                <div className="lg:pb-0 slider-con">
+                <div ref={slideRef} className="lg:pb-0 slider-con">
                     <FlavorSlider />
                 </div>
             </div>
